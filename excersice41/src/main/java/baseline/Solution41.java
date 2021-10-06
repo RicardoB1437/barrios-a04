9fcadcea696b6c2375d7496d396c9e1cfb1456cf
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-10-06 18:10:14 -0400

    41 shell

---
+++ excersice41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ricardo Barrios
+ */
+
+package baseline;
+import java.util.*;
+import java.util.Scanner;
+
+/*
+ * all current methods/classes will be placed in their proper locations after psuedocode is done
+ * they are all here simply for the moment so that i can visualize all code together
+ */
+
+public class Solution41
+{
+    public ArrayList<String> takeNamesFromFile()
+    {
+        //make a reader to file
+        //read in each line and store in array list
+        //return list
+    }
+
+    public void printNamesToFile(ArrayList<String> names)
+    {
+        //make a writer to file
+        //print the contents of the list to file
+    }
+
+    public static void main(String[] args)
+    {
+        //FileReader read = new FileReader();
+
+        //take input from file
+        //ArrayList<String> names = read.takeNamesFromFile();
+
+        //sort list
+        //Collections.sort(names);
+
+        //print sorted list to file
+        //read.printNamesToFile(names);
+    }
+}
