87809372faeb5fb73770ad75148b03598f56728a
RicardoB1437 <ricardoblll@knights.ucf.edu>
2021-10-17 23:18:40 -0400

    46 shell

---
+++ excersice46/src/main/java/baseline/FileInfoReader.java
@@ -0,0 +1,29 @@
+package baseline;
+
+public class FileInfoReader
+{
+    //List<String> fileInfo = new ArrayList<>();
+    //Map<String, Integer> wordCount = new HashMap<>();
+    //List<Map<String, Integer>> allWordsCount = new HashMap<>();
+
+    public void takeInfoFromFile()
+    {
+        //make a reader to file
+        //read in each line and store in array list
+        //List<String> fileInfo = new ArrayList<>();
+    }
+
+    public int countWords()
+    {
+        //this will set the map
+        //go through each line in fileInfo
+        //update the map every time word is found
+    }
+
+    public void printInfoToFile()
+    {
+        //make a writer to file
+        //print the contents to a file
+        //make a max int flag and go through the list of maps
+    }
+}
